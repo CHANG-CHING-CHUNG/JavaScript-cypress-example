a51813219c9568ffd8bc00283182007b6ba86f11
john <john.chang@nexus-techltd.com>
2021-10-04 15:52:49 +0800

    training test completed

--- cypress/integration/training_flow_spec.js
+++ cypress/integration/training_flow_spec.js
@@ -12,6 +12,16 @@ describe('Running a whole training operation', () => {
     cy.get("button#input_sumbit").click()
   }
 
+  const getToday = () => {
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+
+    today = `${yyyy}-${mm}-${dd}`
+    return today
+  }
+
   it('Log in and add a new layer', () => {
     cy.login(username,password)
     cy.visit(Cypress.env("layer_mapping_url"))
@@ -46,19 +56,32 @@ describe('Running a whole training operation', () => {
       }
     )
     cy.get("button#upload").click({force:true})
+    cy.wait(1000)
   })
 
   it("Train models", () => {
-    
+    const today = getToday()
     cy.login(username,password)
     cy.visit(Cypress.env("model_training_url"))
     cy.get("input#busniess_office").type("test-john",{force: true})
-    cy.get("input#name_date").type("2021-09-17",{force: true})
+    cy.get("input#name_date").type(today,{force: true})
     cy.get("input#name_type").type("test-john",{force:true})
     cy.get("select#layer_selector").select("g3").should("have.value","g3")
     cy.get("input#batch_size").clear({force: true}).type(Cypress.env("batch_size"),{force: true})
     cy.get("input#learning_rate").clear({force: true}).type(Cypress.env("learning_rate"),{force: true})
     cy.get("input#epochs").clear({force: true}).type(Cypress.env("epochs"),{force: true})
     cy.get("button#new_job").click({force: true})
+  })
+
+  it("Check status & training history", () => {
+    const today = getToday()
+    cy.login(username,password)
+    cy.visit(Cypress.env("server_status_url"))
+    cy.get("a#train-tab").click({force: true})
+    cy.contains(`test-john_${today}_test-john`)
+    cy.visit(Cypress.env("train_hist_url"))
+    cy.wait(600000)
+    cy.get('a[href="#g3"]').click({force: true})
+    cy.contains(`test-john_${today}_test-john`)
   })
 })
